120b1d014a08ea5a58672fe774d53cfa8ab71662
Burak849 <burakkurtulus018@gmail.com>
2025-04-23 03:37:45 +0300

    regex exercises

--- PracticeCodes/es6.js
+++ PracticeCodes/es6.js
@@ -69,3 +69,148 @@ function removeFirstTwo( list ) {
     return failureItems;
   }
   const failuresList = makeList(result.failure); 
+
+
+
+const bicycle = {
+  gear: 2,
+  setGear(newGear) { return this.gear = newGear; }
+}; // function in object
+bicycle.setGear(3);
+console.log(bicycle.gear);
+
+
+
+class Vegetable { 
+  constructor(name){
+    return this.name = name;
+  }
+}
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // Should display 'carrot'
+
+
+
+class Thermostat{
+  constructor( fahr ){
+    this._fahr = fahr;
+  }
+  //getter
+  get temperature(){
+    return (5 / 9) * (this._fahr - 32);
+  };
+  //setter
+  set temperature(celc){
+    return this._fahr = (celc * 9.0) / 5 + 32;
+  }
+}
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
+
+
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer represents a response from a server
+  let responseFromServer;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+});
+makeServerRequest.then ( (result) => {
+  console.log(result);
+}) // seeing result
+.catch( error => {
+  console.log(error);
+}); // error
+
+
+//! REGEX
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result1 = myRegex.test(myString); // .test() feature
+
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // finding the word "Waldo" you can add more string by adding | operator
+let result2 = waldoRegex.test(waldoIsHiding);
+
+let myString1 = "freeCodeCamp";
+let fccRegex = /freecodecamp/i; // by adding i to it its avoding the uppercase lowercase issues 
+let result3 = fccRegex.test(myString);
+
+
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; 
+let result4 = extractStr.match(codingRegex); // .match() method
+
+
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /Twinkle/gi; 
+let result5 = twinkleStar.match(starRegex); // for repeated words
+
+
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/;
+let result6 = unRegex.test(exampleStr);
+
+
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/ig; // all the vowels
+let result7 = quoteSample.match(vowelRegex); 
+
+let quoteSample1 = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/ig; // all the letter between a-z
+let result8 = quoteSample.match(alphabetRegex); 
+
+let difficultSpelling = "Mississippi";
+let myRegex1 = /s+/g; // if you want to find all the s letters in the sentence, * means => 0 or more times + was 1 or more times and i dont need /ig
+let result9 = difficultSpelling.match(myRegex);
+
+let text = "<h1>Winter is coming</h1>";
+let myRegex2 = /<.*?>/;  // . = for any character, * is from 0 to more, ? =
+let result10 = text.match(myRegex);
+
+// ^ => searches for the first word of the string, $ => end of the string !!! ^ must be in the beggining of the regex and $ must be last one
+
+let quoteSample3 = "The five boxing wizards jump quickly.";
+let alphabetRegexV2 = /\w/g; // short hand of all the words and numbers, All must be "g" flag
+// if you make it \W it will inlcudes nonalphanumeric numbers (%, .4 ...)
+let resultt = quoteSample.match(alphabetRegexV2).length;
+
+// \d => shothad of [0-9], \D => means ^[0-9]
+
+
+// Usernames can only use alphanumeric characters.
+// The only numbers in the username have to be at the end. There can be zero or more of them at the end. Username cannot start with the number.
+// Username letters can be lowercase and uppercase.
+// Usernames have to be at least two characters long. A two-character username can only use alphabet letters as characters.
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i; 
+let Result = userCheck.test(username);
+
+// \s => whitespace finder also needs "g" flag , \S => searches for non-whitespaces
+// you can specify the lower and upper number by using => {} first value in starting to last is second vaule, you can make it for one match by using one value
+
+// A positive lookahead will look to make sure the element in the search pattern is there, 
+// but won't actually match it. A positive lookahead is used as (?=...) where the ... is the required part that is not matched.
+
+// On the other hand, a negative lookahead will look to make sure the element in the search pattern is not there. 
+// A negative lookahead is used as (?!...) where the ... is the pattern that you do not want to be there. The rest of the pattern
+//  is returned if the negative lookahead part is not present.
+
+let sampleWord = "astronaut";
+let pwRegex = /(?=\w{6,})(?=\w*\d{2})/; // match passwords that are greater than 5 characters long, and have two consecutive digits
+let result_ = pwRegex.test(sampleWord);
+
+let str = "one two three";
+let fixRegex = /(\w+)\s(\w+)\s(\w+)/; // selecting words for replacing method 
+let replaceText = "$3 $2 $1"; // the queue of the strings
+let result_2 = str.replace(fixRegex, replaceText);
+
+
+let hello = "   Hello, World!  ";
+let wsRegex = /^\s+|\s+$/g;
+let result_3 = hello.replace(wsRegex,""); // to replace the whole string
